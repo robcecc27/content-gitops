88ac44385d703e40ac5da75241ac715c1fcf532f
robcecc27 <rob.cecchini@gmail.com>
2022-06-03 21:50:29 -0500

    changed python return message

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Python Say hello from inside a Docker container!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
